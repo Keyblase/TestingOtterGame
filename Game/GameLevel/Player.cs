f41e795ddfcc1f4df3c1a9b5de6585eb5be5f82f
Nicolas <nico_sansoares@hotmail.com>
2019-11-10 12:48:14 -0300

    First Update

---
+++ Game/GameLevel/Player.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otter
+{
+    class Player : Entity
+    {
+        public float MoveSpeed;
+
+        public const float MoveSpeedSlow = 5;
+        public const float MoveSpeedFast = 10;
+
+        public Player(float x, float y) : base(x, y)
+        {
+            Sprite = Image.CreateRectangle(25, 25, Color.White);
+            Sprite.CenterOrigin();
+            AddGraphic(Sprite);
+            MoveSpeed = MoveSpeedSlow;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            if (Input.KeyDown(Key.W))
+            {
+                Y -= MoveSpeed;
+            }
+            if (Input.KeyDown(Key.S))
+            {
+                Y += MoveSpeed;
+            }
+            if (Input.KeyDown(Key.A))
+            {
+                X -= MoveSpeed;
+            }
+            if (Input.KeyDown(Key.D))
+            {
+                X += MoveSpeed;
+            }
+
+            if (Input.KeyPressed(Key.Space))
+            {
+                // If the Entity is currently slow,
+                if (MoveSpeed == MoveSpeedSlow)
+                {
+                    // Set the Entity to fast
+                    MoveSpeed = MoveSpeedFast;
+                    Graphic.Color = Color.Green;
+                }
+                else
+                {
+                    MoveSpeed = MoveSpeedSlow;
+                    // And make its Color white(normal State).
+                    Graphic.Color = Color.White;
+                }
+            }
+        }
+
+        public Image Sprite { get; }
+    }
+
+}
---
+++ Game/Program.cs
@@ -0,0 +1,22 @@
+﻿using Otter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLevels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var link = new Game("Sword Nicolas  Online");
+            Player player = new Player(Game.Instance.HalfWidth, Game.Instance.HalfHeight);
+
+            var levelOne = new Scene();
+            levelOne.Add(player);
+            link.Start(levelOne);
+        }
+    }
+}
